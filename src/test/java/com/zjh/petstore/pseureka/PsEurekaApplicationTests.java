66a6016cec977bfe911df6ee40296c98cbc5bef5
zhaomsdemo <zhaomsdemo@gmail.com>
2020-07-23 10:38:04 -0700

    init

---
+++ src/test/java/com/zjh/petstore/pseureka/PsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.petstore.pseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
